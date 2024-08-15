f304a2f02f349de1e356e1508ce90de0571b0e0a
wain-code <minhthu310121@gmail.com>
2024-08-15 10:47:05 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+         System.out.println("Hello Huynh Thu");
+         System.out.println("Welcome to the DHHTTT19A");
+    }
+}
